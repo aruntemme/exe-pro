3a9fb6c7be92cd85f7533a63afdb6a43d74e33bd
aruntemme <arungauthamk@gmail.com>
2021-07-31 15:56:46 +0530

    hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
+const ANIMALS = ['dog', 'cat', 'bird', 'fish'];
+
 const SearchParams = () => {
   const [location, setLocation] = useState('Salem, Tamilnadu');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,38 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animalDropdown">
+          Animal
+          <select
+            value={animal}
+            id="animalDropdown"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breedlDropdown">
+          Breed
+          <select
+            value={breed}
+            id="breedDropdown"
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option></option>
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
